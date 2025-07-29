a6f3d426d5a6b84ad88a041db28ebb077fba17eb
solonirinafalda335-star <solonirinafalda335@gmail.com>
2025-07-29 11:29:18 +0300

    Fix model to command

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import { CohereClient } from 'cohere-ai';
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 10000;
 
 app.use(cors());
 app.use(express.json());
